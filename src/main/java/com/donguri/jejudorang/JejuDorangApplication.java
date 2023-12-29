971736b7aba9bf5a79d7d53febc39351fcb95e52
dongur2 <holymolyguri@gmail.com>
2023-12-29 18:43:44 +0900

    first commit

---
+++ src/main/java/com/donguri/jejudorang/JejuDorangApplication.java
@@ -0,0 +1,13 @@
+package com.donguri.jejudorang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JejuDorangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JejuDorangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/donguri/jejudorang/JejuDorangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donguri.jejudorang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JejuDorangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
