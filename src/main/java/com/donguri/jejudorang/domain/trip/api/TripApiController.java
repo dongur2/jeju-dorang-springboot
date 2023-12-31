290a06f516a9f870f2e45260b11fa805d4e42b4c
dongur2 <holymolyguri@gmail.com>
2024-01-01 02:23:38 +0900

    build: netty-resolver-dns-native-macos dependency 추가 Webflux MacOS 관련 에러 DnsServerAddressStreamProviders 해결 위한 dependency 추가 TripApiController WebClient로 임시 리팩토링

--- src/main/java/com/donguri/jejudorang/domain/trip/api/TripApiController.java
+++ src/main/java/com/donguri/jejudorang/domain/trip/api/TripApiController.java
@@ -2,17 +2,14 @@ package com.donguri.jejudorang.domain.trip.api;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.DefaultUriBuilderFactory;
 
-import java.io.UnsupportedEncodingException;
-import java.util.Map;
+import java.util.Arrays;
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -21,18 +18,28 @@ public class TripApiController {
     @Value("${jeju-api-key}") // application-API-KEY.properties
     private String apiKey;
     private final String baseUrl = "https://api.visitjeju.net/vsjApi/contents/searchList";
-    private final String apiKeyParam = "?apiKey=";
-    private final String defualtQueryParams = "&locale=kr&category=c1&category=c2&category=c4";
-
-    private String setUrl() throws UnsupportedEncodingException {
-        return baseUrl + apiKeyParam + apiKey + defualtQueryParams;
-    }
+    private final String locale = "kr";
+    private final List<String> categories = Arrays.asList("c1", "c2", "c4");
 
     @GetMapping("/trip/api/data")
-    public ResponseEntity<?> fetch() throws UnsupportedEncodingException {
-        RestTemplate restTemplate = new RestTemplate();
-        HttpEntity<?> entity = new HttpEntity<>(new HttpHeaders());
-        ResponseEntity<Map> resultMap = restTemplate.exchange(setUrl(), HttpMethod.GET, entity, Map.class);
-        return resultMap;
+    public String fetch() {
+        DefaultUriBuilderFactory factory = new DefaultUriBuilderFactory(baseUrl);
+        factory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.VALUES_ONLY);
+
+        WebClient webClient = WebClient.builder()
+                    .uriBuilderFactory(factory)
+                    .baseUrl(baseUrl)
+                    .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                    .build();
+
+        return webClient.get()
+                    .uri(uriBuilder -> uriBuilder
+                            .queryParam("apiKey", apiKey)
+                            .queryParam("locale", locale)
+                            .queryParam("category", categories.toArray())
+                            .build())
+                    .retrieve()
+                    .bodyToMono(String.class)
+                    .block();
     }
 }
