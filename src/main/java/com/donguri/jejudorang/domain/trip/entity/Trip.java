25af26a69da5ecf3bd4b448b6b28779c7e5ea717
dongur2 <holymolyguri@gmail.com>
2023-12-31 19:22:26 +0900

    feat: Trip Entity 기본 컬럼 추가

---
+++ src/main/java/com/donguri/jejudorang/domain/trip/entity/Trip.java
@@ -0,0 +1,23 @@
+package com.donguri.jejudorang.domain.trip.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Trip {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "trip_id")
+    private Long id; // PK
+    private String placeId; // CNTS_300000000015996
+    private String category; // 쇼핑
+    private String name; // 장소 이름
+    private String introduction; // 소개
+    private String address; // 도로명 주소: 제주특별자치도 제주시 구좌읍 구좌로 51-1
+    private String tel; // 064-766-3080
+    private String tags; // 구좌읍, 세화리, 소품샵, 핸드메이드, 잡화, 레진공예
+    private String thumbnail; // url
+    private String image; // url
+    private int likeCount; // 북마크 수
+
+}
