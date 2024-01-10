edd1f30ad6ed18abb006b449740152b3a91acc6a
dongur2 <holymolyguri@gmail.com>
2024-01-10 21:47:38 +0900

    feat: 커뮤니티 수정폼 PUT method 사용 위한 HiddenHttpMethodFilter 추가, 관련 설정 추가

--- src/main/java/com/donguri/jejudorang/JejuDorangApplication.java
+++ src/main/java/com/donguri/jejudorang/JejuDorangApplication.java
@@ -2,7 +2,9 @@ package com.donguri.jejudorang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -10,6 +12,11 @@ public class JejuDorangApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JejuDorangApplication.class, args);
+	}
+
+	@Bean
+	public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+		return new HiddenHttpMethodFilter();
 	}
 
 }
