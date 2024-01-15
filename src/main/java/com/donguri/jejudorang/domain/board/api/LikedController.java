9e08a883949ad38f211caac953d76d26c62bcf0f
dongur2 <holymolyguri@gmail.com>
2024-01-15 20:01:26 +0900

    feat: 커뮤니티 상세글 뷰에서 북마크 메서드 추가 로그인하지 않았을 경우 북마크 불가능 조건 추가

--- src/main/java/com/donguri/jejudorang/domain/board/api/LikedController.java
+++ src/main/java/com/donguri/jejudorang/domain/board/api/LikedController.java
@@ -6,10 +6,10 @@ import com.donguri.jejudorang.domain.user.entity.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -18,15 +18,16 @@ public class LikedController {
     @Autowired
     private LikedService likedService;
 
-    @ResponseStatus(HttpStatus.OK)
     @PutMapping("/board/detail/{boardId}/updateLiked")
-    public void updateBoardLikedState(@AuthenticationPrincipal User nowUser,
-                                       @PathVariable("boardId") Long boardId) {
+    public ResponseEntity<String> updateBoardLikedState(@AuthenticationPrincipal User nowUser,
+                                                        @PathVariable("boardId") Long boardId) {
         if (nowUser == null) {
             log.info("nowUser is NULL");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("비회원은 북마크 불가능");
         } else {
             log.info("nowUser={}", nowUser.toString());
             likedService.updateBoardLikedState(nowUser.getId(), boardId);
+            return ResponseEntity.ok("북마크 완료");
         }
     }
 }
