4e20a293f0364e03bfbb8106b0d951a6c556f52e
dongur2 <holymolyguri@gmail.com>
2024-01-23 23:27:03 +0900

    feat: UserController JWT login 임시 메서드 추가

--- src/main/java/com/donguri/jejudorang/domain/user/api/UserController.java
+++ src/main/java/com/donguri/jejudorang/domain/user/api/UserController.java
@@ -1,11 +1,18 @@
 package com.donguri.jejudorang.domain.user.api;
 
 import com.donguri.jejudorang.domain.user.dto.LoginRequest;
+import com.donguri.jejudorang.domain.user.entity.User;
+import com.donguri.jejudorang.domain.user.repository.UserRepository;
 import com.donguri.jejudorang.global.util.JwtUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -16,7 +23,9 @@ public class UserController {
     private final AuthenticationManager authenticationManager;
 
     @Autowired
-    private final JwtUtil jwtUtil;
+    UserRepository userRepository;
+
+    private JwtUtil jwtUtil;
 
     public UserController(AuthenticationManager authenticationManager, JwtUtil jwtUtil) {
         this.authenticationManager = authenticationManager;
@@ -24,8 +33,18 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public String login(LoginRequest request) {
+    public String login(LoginRequest loginReq) {
+        try {
+            Authentication authentication =
+                    authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginReq.externalId(), loginReq.password()));
+            String externalId = authentication.getName();
+            User user = userRepository.findByExternalId(externalId).get();
+            String token = jwtUtil.createToken(user);
 
-        return null;
+            return HttpStatus.OK.toString();
+
+        } catch (Exception e){
+            return HttpStatus.BAD_REQUEST.toString();
+        }
     }
 }
