70932d70a86aaee139983b3547438f3a31827b80
dongur2 <holymolyguri@gmail.com>
2024-01-24 02:05:02 +0900

    feat: UserDetailsServiceImpl 코드 추가

--- src/main/java/com/donguri/jejudorang/global/config/UserDetailsServiceImpl.java
+++ src/main/java/com/donguri/jejudorang/global/config/UserDetailsServiceImpl.java
@@ -1,15 +1,26 @@
 package com.donguri.jejudorang.global.config;
 
+import com.donguri.jejudorang.domain.user.entity.User;
+import com.donguri.jejudorang.domain.user.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
 
     @Override
+    @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        User user = userRepository.findByExternalId(username)
+                .orElseThrow(() -> new EntityNotFoundException("다음 아이디에 해당하는 유저가 없습니다 : " + username));
+
+        return UserDetailsImpl.build(user);
     }
 }
