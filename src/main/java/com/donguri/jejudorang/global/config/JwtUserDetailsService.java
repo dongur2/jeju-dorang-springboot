e9ebcbf29e4870a7d78ec55e879af7084d5903ae
dongur2 <holymolyguri@gmail.com>
2024-01-25 03:34:14 +0900

    faet: JwtAuthEntryPoint 생성

--- src/main/java/com/donguri/jejudorang/global/config/JwtUserDetailsService.java
+++ src/main/java/com/donguri/jejudorang/global/config/JwtUserDetailsService.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -23,6 +24,7 @@ public class JwtUserDetailsService implements UserDetailsService {
     }
 
     @Override
+    @Transactional
     public UserDetails loadUserByUsername(String externalId) throws UsernameNotFoundException {
         Optional<User> dbUser = userRepository.findByExternalId(externalId);
         log.info("Fetched User : {} by {}", dbUser, externalId);
@@ -30,6 +32,7 @@ public class JwtUserDetailsService implements UserDetailsService {
                 .orElseThrow(() -> new UsernameNotFoundException("해당 아이디를 가진 유저가 없습니다: " + externalId));
     }
 
+    @Transactional
     public UserDetails loadUserById(Long id) {
         Optional<User> dbUser = userRepository.findById(id);
         log.info("Fetched User : {} by {}", dbUser, id);
--- src/main/java/com/donguri/jejudorang/global/config/SecurityConfig.java
+++ src/main/java/com/donguri/jejudorang/global/config/SecurityConfig.java
@@ -40,6 +40,7 @@ public class SecurityConfig {
 
         return authenticationManagerBuilder.build();
     }
+
     /*
     * securityFilterChain
     *
