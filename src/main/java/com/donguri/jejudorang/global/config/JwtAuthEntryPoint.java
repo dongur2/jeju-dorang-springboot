d1f213edebe473c4e30461cb7bb1b60ea49913b3
dongur2 <holymolyguri@gmail.com>
2024-01-26 03:56:00 +0900

    cmt: 주석 추가

--- src/main/java/com/donguri/jejudorang/global/config/JwtAuthEntryPoint.java
+++ src/main/java/com/donguri/jejudorang/global/config/JwtAuthEntryPoint.java
@@ -14,6 +14,10 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+/*
+* Spring Security에서 사용자가 인증되지 않았을 때 호출
+* - 인증되지 않은 사용자가 보호된 리소스에 접근을 시도할 때 호출
+* */
 @Slf4j
 @Component
 public class JwtAuthEntryPoint implements AuthenticationEntryPoint {
@@ -24,15 +28,18 @@ public class JwtAuthEntryPoint implements AuthenticationEntryPoint {
 
         log.error("Unauthorized error: {}", authException.getMessage());
 
+        // HTTP 응답 헤더 설정: JSON, 상태 코드 401(unauthorized)
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 
+        // HTTP 응답 바디 설정: 상태 코드 401, 에러 메세지, 요청 경로 포함 맵 생성
         final Map<String, Object> body = new HashMap<>();
         body.put("status", HttpServletResponse.SC_UNAUTHORIZED);
         body.put("error", "Unauthorized");
         body.put("message", authException.getMessage());
         body.put("path", request.getServletPath());
 
+        // 생성한 맵을 ObjectMapper를 사용해 JSON형식으로 변환해 응답 스트림에 전송
         final ObjectMapper mapper = new ObjectMapper();
         mapper.writeValue(response.getOutputStream(), body);
 
--- src/main/java/com/donguri/jejudorang/global/config/SecurityConfig.java
+++ src/main/java/com/donguri/jejudorang/global/config/SecurityConfig.java
@@ -37,16 +37,16 @@ public class SecurityConfig {
 
     @Bean
     public DaoAuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(); // 사용자 정보 조회, 인증 수행 구현체
 
-        authProvider.setUserDetailsService(jwtUserDetailsService);
-        authProvider.setPasswordEncoder(passwordEncoder());
+        authProvider.setUserDetailsService(jwtUserDetailsService); // DB 사용자 정보 조회에 사용될 UserDetailsService 설정 - 사용자 제공한 인증 정보와 데이터베이스에 저장된 사용자 정보를 비교해 인증
+        authProvider.setPasswordEncoder(passwordEncoder()); // 사용자 비밀번호 인코딩에 사용될 PasswordEncoder 설정
 
         return authProvider;
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception { // 사용자가 제공한 권한 기반 인증 -> Authentication 오브젝트 반환
         return authConfig.getAuthenticationManager();
     }
 
@@ -99,27 +99,11 @@ public class SecurityConfig {
                                                 "/css/**", "/img/**").permitAll()
                                         .anyRequest().authenticated()));
 
-        http.authenticationProvider(authenticationProvider());
+        http.authenticationProvider(authenticationProvider()); // 사용자의 인증 정보를 제공하는 authenticationProvider 설정: 사용자 로그인 정보 기반 인증 수행
 
-        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class); // Spring Security Filter Chain에 사용자 정의 필터 추가(JWT 인증 필터)
 
         return http.build();
     }
-
-    /*
-    * PasswordEncoder
-    *
-    * Service interface for encoding passwords.
-    * The preferred implementation is BCryptPasswordEncoder.
-    *
-    * > PasswordEncoderFactories: Used for creating PasswordEncoder instances
-    * >> createDelegatingPasswordEncoder : creates a DelegatingPasswordEncoder with default mappings.
-    * >>> The mappings current are:
-    *       bcrypt - BCryptPasswordEncoder (Also used for encoding)
-    * */
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-//    }
 
 }
