f9eec29aeadb90c7e8da9415715ff6bf50be62a6
dongur2 <holymolyguri@gmail.com>
2024-01-26 01:00:03 +0900

    feat: RedisConfig 추가

---
+++ src/main/java/com/donguri/jejudorang/global/config/RedisConfig.java
@@ -0,0 +1,42 @@
+package com.donguri.jejudorang.global.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${redis.host}")
+    private String host;
+
+    @Value("${redis.port}")
+    private int port;
+
+    /*
+    * Redis DB 연결 생성하는 팩토리 설정
+    * */
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
+    }
+
+    /*
+    * RedisTemplate에 RedisConnectionFactory 설정 -> 커넥션 제공
+    * > RedisTemplate: Redis 데이터 조작을 위한 템플릿 클래스
+    * */
+    @Bean
+    public RedisTemplate<String, String> redisTemplate() {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+
+        return redisTemplate;
+    }
+}
