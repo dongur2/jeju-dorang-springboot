598b620d7bb621b20cdcb18bdb55ebd189391ca5
dongur2 <holymolyguri@gmail.com>
2024-02-16 05:54:05 +0900

    design: js로 받아온 알림 목록 헤더에 표시
    - NotificationResponse에 리다이렉트 위한 type 추가, 알림 목록 표시 구분 위한 isChecked, createdAt 프로퍼티 추가
    - 알림 목록 헤더 ul 리스트 팝오버로 추가 (임시)

--- src/main/java/com/donguri/jejudorang/domain/notification/dto/NotificationResponse.java
+++ src/main/java/com/donguri/jejudorang/domain/notification/dto/NotificationResponse.java
@@ -1,21 +1,32 @@
 package com.donguri.jejudorang.domain.notification.dto;
 
+import com.donguri.jejudorang.domain.community.entity.BoardType;
 import com.donguri.jejudorang.domain.notification.entity.Notification;
+import com.donguri.jejudorang.global.common.DateFormat;
 import lombok.Builder;
 
 @Builder
 public record NotificationResponse(
         Long userId,
         String content,
+        String type,
         Long postId,
-        String isChecked
+        String isChecked,
+        String createdAt
 ) {
     public static NotificationResponse from(Notification notification) {
+        String type = "parties";
+        if(notification.getPost().getType().equals(BoardType.CHAT)) {
+            type = "chats";
+        }
+
         return NotificationResponse.builder()
                 .userId(notification.getOwner().getId())
                 .content(notification.getContent())
+                .type(type)
                 .postId(notification.getPost().getId())
                 .isChecked(notification.getIsChecked().name())
+                .createdAt(DateFormat.calculateTime(notification.getCreatedAt()))
                 .build();
     }
 }
