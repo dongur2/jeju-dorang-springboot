62b3a506f9ded78cec6d103285b2d66a2e0ac91e
dongur2 <holymolyguri@gmail.com>
2024-02-15 21:19:48 +0900

    feat: Notification Entity 추가
    - 읽음 상태 컬럼 위한 IsChecked Enum (CHECKED, NOT_YET) 추가

--- src/main/java/com/donguri/jejudorang/domain/notification/entity/Notification.java
+++ src/main/java/com/donguri/jejudorang/domain/notification/entity/Notification.java
@@ -1,11 +1,37 @@
 package com.donguri.jejudorang.domain.notification.entity;
 
-import jakarta.persistence.Entity;
+import com.donguri.jejudorang.domain.user.entity.User;
+import com.donguri.jejudorang.global.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
 @NoArgsConstructor
-public class Notification {
+public class Notification extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @ManyToOne(cascade = CascadeType.REMOVE)
+    @JoinColumn(nullable = false, name = "user_id")
+    User owner; // 회원 탈퇴시 함께 삭제
+
+    @Column(nullable = false)
+    String content;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    IsChecked isChecked; // 읽음 상태
+
+    @Builder
+    public Notification(User owner, String content, IsChecked isChecked) {
+        this.owner = owner;
+        this.content = content;
+        this.isChecked = isChecked;
+    }
+
 }
