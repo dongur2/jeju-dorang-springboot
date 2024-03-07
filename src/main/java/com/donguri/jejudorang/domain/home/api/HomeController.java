ad8c014c8efcffbe47d7969bce906ce49018ef9e
dongur2 <holymolyguri@gmail.com>
2024-03-08 01:01:00 +0900

    feat: Swagger 관련 설정사항 추가
    - SecurityConfig에 Swagger관련 매핑 인증 무시 추가
    - SwaggerConfig 설정 파일 추가: 후에 JWT 수정 필요
    - HomeController Swagger API 추가

--- src/main/java/com/donguri/jejudorang/domain/home/api/HomeController.java
+++ src/main/java/com/donguri/jejudorang/domain/home/api/HomeController.java
@@ -1,5 +1,7 @@
 package com.donguri.jejudorang.domain.home.api;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -7,13 +9,17 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @Controller
 @RequestMapping("/")
+@Tag(name = "Home", description = "메인 홈 화면 리턴")
 public class HomeController {
+
     @GetMapping
+    @Operation(summary = "홈 화면", description = "홈 화면 출력")
     public String home() {
         return "home";
     }
 
     @PostMapping
+    @Operation(summary = "홈 화면", description = "홈 화면 출력하도록 리다이렉트")
     public String redirectHome() {
         return "redirect:/";
     }
---
+++ src/main/java/com/donguri/jejudorang/global/config/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package com.donguri.jejudorang.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        String jwt = "JWT";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt);
+        Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
+                .name(jwt)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT"));
+
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo())
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("제주도랑 API Doc")
+                .description("제주도랑 API 명세서")
+                .version("1.0.0");
+    }
+
+}
