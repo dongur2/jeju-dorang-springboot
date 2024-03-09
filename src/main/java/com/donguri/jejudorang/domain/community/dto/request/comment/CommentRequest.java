32bfc3599ffc59d858953da0c5f790bbb586251d
dongur2 <holymolyguri@gmail.com>
2024-03-09 10:42:57 +0900

    feat: CommentController Swagger 애노테이션 추가
    - 댓글/대댓글 작성 컨트롤러 String 반환 -> ResponseEntity 반환으로 변경
    - 댓글/대댓글 작성 요청 js 비동기 요청으로 변경

--- src/main/java/com/donguri/jejudorang/domain/community/dto/request/comment/CommentRequest.java
+++ src/main/java/com/donguri/jejudorang/domain/community/dto/request/comment/CommentRequest.java
@@ -1,16 +1,19 @@
 package com.donguri.jejudorang.domain.community.dto.request.comment;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 @Builder
+@Schema(description = "댓글 작성 요청 정보")
 public record CommentRequest(
 
+        @Schema(description = "게시글 ID")
         Long postId,
 
+        @Schema(name = "content", description = "50자를 초과하지 않는 댓글 내용")
         @Size(max = 50, message = "댓글은 50자를 초과할 수 없습니다.")
         @NotBlank(message = "댓글 내용을 입력해주세요.")
         String content
-) {
-}
+) {}
